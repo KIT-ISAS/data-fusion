d25f0f1b93dc8759670d6876b87d8e03e11ea0ed
Benjamin Alt <benjamin_alt@outlook.com>
2018-05-08 22:16:57 +0200

    Simplified CI interface

--- algorithms/covariance_intersection.py
+++ algorithms/covariance_intersection.py
@@ -1,8 +1,8 @@
-from estimate import Estimate
 from enum import Enum
 from scipy.optimize import minimize
 from numpy.linalg import inv, det
 import numpy as np
+
 
 class PerformanceCriterion(Enum):
     TRACE = 1,
@@ -13,15 +13,14 @@ class CovarianceIntersection(object):
     def __init__(self, performance_criterion):
         self.performance_criterion = det if performance_criterion == PerformanceCriterion.DETERMINANT else np.trace
 
-    def fuse(self, a, b):
-        omega = self.optimize_omega(a, b)
-        p_cc = inv(omega * inv(a.cov) + (1 - omega) * inv(b.cov))
-        c = p_cc * (omega * inv(a.cov) * a + (1 - omega) * inv(b.cov) * b)
-        return Estimate(c, p_cc)
+    def fuse(self, mean_a, cov_a, mean_b, cov_b):
+        omega = self.optimize_omega(cov_a, cov_b)
+        p_cc = inv(omega * inv(cov_a) + (1 - omega) * inv(cov_b))
+        c = p_cc * (omega * inv(cov_a) * mean_a + (1 - omega) * inv(cov_b) * mean_b)
+        return c, p_cc
 
-    def optimize_omega(self, a, b):
-
+    def optimize_omega(self, cov_a, cov_b):
         def optimize_fn(omega):
-            self.performance_criterion(inv(omega * inv(a.cov) + (1 - omega) * inv(b.cov)))
+            self.performance_criterion(inv(omega * inv(cov_a) + (1 - omega) * inv(cov_b)))
         omega_0 = np.array([1.0])
         return minimize(optimize_fn, omega_0)[0]
--- estimate.py
+++
@@ -1,5 +0,0 @@
-class Estimate(object):
-
-    def __init__(self, mean, cov):
-        self.mean = mean
-        self.cov = cov
