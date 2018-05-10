aaaedd4c0f88011702a41d271ad5237bbd7564ac
Benjamin Alt <benjamin_alt@outlook.com>
2018-05-10 14:29:59 +0200

    First tentative ICI implementation

---
+++ algorithms/inverse_covariance_intersection.py
@@ -0,0 +1,33 @@
+"""
+Inverse covariance intersection according to http://discovery.ucl.ac.uk/135541/
+"""
+
+from enum import Enum
+from scipy.optimize import minimize
+from numpy.linalg import inv, det
+import numpy as np
+
+
+class PerformanceCriterion(Enum):
+    TRACE = 1,
+    DETERMINANT = 2
+
+
+class InverseCovarianceIntersection(object):
+    def __init__(self, performance_criterion=PerformanceCriterion.DETERMINANT):
+        self.performance_criterion = det if performance_criterion == PerformanceCriterion.DETERMINANT else np.trace
+
+    def fuse(self, mean_a, cov_a, mean_b, cov_b):
+        omega = self.optimize_omega(cov_a, cov_b)
+        cov = inv(inv(cov_a) + inv(cov_b) - inv(np.multiply(omega, cov_a) + np.multiply(1 - omega, cov_b)))
+        T = inv(np.multiply(omega, cov_a) + np.multiply(1-omega, cov_b))
+        K = np.dot(cov, inv(cov_a) - np.multiply(omega, T))
+        L = np.dot(cov, inv(cov_b) - np.multiply(1 - omega, T))
+        mean = np.dot(K, mean_a) + np.dot(L, mean_b)
+        return mean, cov
+
+    def optimize_omega(self, cov_a, cov_b):
+        def optimize_fn(omega):
+            return self.performance_criterion(inv(np.multiply(omega, inv(cov_a)) + np.multiply(1 - omega, inv(cov_b))))
+        omega_0 = np.array([1.0])
+        return minimize(optimize_fn, omega_0).x[0]
